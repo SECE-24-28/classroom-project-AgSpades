bba2344ea663dc85a866104f0e86b956c67e7f93
agspades <psaumyajit@outlook.com>
2025-12-08 11:51:01 +0530

    feat: remove unused press function from App component

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -2,11 +2,6 @@ import Header from "./components/Header";
 import Content from "./components/Content";
 function App() {
 
-  const press = () => {
-    let bs = ["John", "David", "Vimal"];
-    let num = Math.floor(Math.random() * 3);
-    console.log(bs[num]);
-  }
   return (
     <>
       <Header></Header>
