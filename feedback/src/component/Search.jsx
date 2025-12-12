12f8c3e0792bd2a833f4b25221b955acdebd592d
agspades <psaumyajit@outlook.com>
2025-12-12 11:33:46 +0530

    feat: remove duplicate post ID display in Search component

--- feedback/src/component/Search.jsx
+++ feedback/src/component/Search.jsx
@@ -12,7 +12,6 @@ const Search = () => {
         (search ? searchResult : posts).map((post) =>
           <div key={post.id}>
             <h3>{post.id}</h3>
-            <h3>{post.id}</h3>
             <Link to={`/post/${post.id}`}>{post.title}</Link>
 
             <p>{post.body}</p>
