f78599ce6efa517c0e4e5db71cdd280cb2c5b406
agspades <psaumyajit@outlook.com>
2025-12-12 09:57:53 +0530

    feat: integrate react-router for navigation and update dependencies

--- feedback/src/App.jsx
+++ feedback/src/App.jsx
@@ -1,22 +1,26 @@
-import { useState } from 'react'
 import './App.css'
 import Home from './component/Home'
 import Search from './component/Search';
 import AddPost from './component/AddPost';
+import { Route, Routes, Link } from 'react-router-dom';
+import { DataProvider } from './context/DataContext';
 
 function App() {
-  const [activeTab, setActiveTab] = useState('home');
-
   return (
-
-    <div>
-      <AddPost />
-      <div className='abc'>
-        <button id="homebtn" onClick={() => setActiveTab('home')}>Home</button>
-        <button id="searchbtn" onClick={() => setActiveTab('search')}>Search</button>
-      </div>
-      {activeTab === 'home' ? <Home /> : <Search />}
-    </div>
+    <>
+      <ol>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/search">Search</Link></li>
+        <li><Link to="/newpost">NewPost</Link></li>
+      </ol>
+      <DataProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/newpost" element={<AddPost />} />
+        </Routes>
+      </DataProvider>
+    </>
   )
 }
 
