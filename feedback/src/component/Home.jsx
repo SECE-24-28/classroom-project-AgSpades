fbab18a34c4f7efa2831ea4fe59ccff415b3fe9d
agspades <psaumyajit@outlook.com>
2025-12-12 10:00:17 +0530

    feat: import Search component and format code in Home component

--- feedback/src/component/Home.jsx
+++ feedback/src/component/Home.jsx
@@ -1,20 +1,21 @@
 import { useContext } from 'react'
 import { DataContext } from '../context/DataContext.jsx'
-
+import Search from './Search.jsx'
 const Home = () => {
-  const {posts} = useContext(DataContext)
+  const { posts } = useContext(DataContext)
   return (
     <>
-    {
-      posts.map((post)=>
-        <div key={post.id}>
-          <h3>{post.id}</h3>
-          <h4>{post.title}</h4>
-          <p>{post.body}</p>
-          <hr/>
-        </div>
-      )
-     }
+      {/* <Search /> */}
+      {
+        posts.map((post) =>
+          <div key={post.id}>
+            <h3>{post.id}</h3>
+            <h4>{post.title}</h4>
+            <p>{post.body}</p>
+            <hr />
+          </div>
+        )
+      }
     </>
   )
 }
